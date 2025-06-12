2f68d27b71436a343ebf720fb98305e77bb0eee7
RayVa0 <evaone0001@gmail.com>
2025-06-12 10:45:26 +0300

    update

--- Main_PC_scripts/imagefetch.sh
+++ Main_PC_scripts/imagefetch.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 dir="${1:-.}"
 pic=$(find "$dir" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.bmp" -o -iname "*.jpeg" -o -iname "*.webp" \) | shuf -n 1)
 
 if [ -n "$pic" ]; then
-    chafa -c full "$pic"
+    chafa -O 0 -c full "$pic"
 else
     echo "No image files found in $dir"
 fi
