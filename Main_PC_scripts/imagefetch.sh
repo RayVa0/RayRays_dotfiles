b702334bf14a914844a75c199bd7850b67148fbd
RayVa0 <evaone0001@gmail.com>
2025-07-01 21:59:26 +0300

    script push

--- Main_PC_scripts/imagefetch.sh
+++ Main_PC_scripts/imagefetch.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
+#The slop, only I know how it works haha
 
 dir="$1"
 pic=$(find "$dir" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.tiff" -o -iname "*.bmp" -o -iname "*.jpeg" -o -iname "*.webp" \) | shuf -n 1)
@@ -17,16 +18,18 @@ fi
 
 if [ -n "$pic" ]; then
     if [ -n "$2" ]; then
-        if [ $2 -eq 1 ]; then
+        if [ $2 -eq 1 ]; then #put 2 if you want half terminal formating and a "from" section
             chafa -O 0 -c $color --view-size=x$lines4 "$pic"
             echo -n "From: "
             ~/Documents/code/experiments/find_dir "$pic" && echo
-        elif [ $2 -eq 2 ]; then 
+        elif [ $2 -eq 2 ]; then  #put 2 if you want half terminal formating and a "from" and "viewing" section 
             chafa -O 0 -c $color "$pic"
             echo -n "You are viewing "
             ~/Documents/code/experiments/find_file "$pic"
             echo -n ", from "
              ~/Documents/code/experiments/find_dir "$pic" && echo
+        elif [ $2 -eq 3 ]; then #terminal formating and no bells and whistles 
+             chafa -O 0 -c $color --view-size=x$lines4 "$pic"
         fi
     else
         chafa -O 0 -c $color "$pic"
