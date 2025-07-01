edfb5c78da4380f2d91ec3871800cc9f65624297
RayVa0 <evaone0001@gmail.com>
2025-07-01 18:50:19 +0300

    script push

---
+++ Main_PC_scripts/update_dotfiles.fish
@@ -0,0 +1,11 @@
+cp -r ~/.config/kitty/ ~/.config/fastfetch/ ~/.config/fish ~/.config/hypr ~/.config/mpv -t ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
+cp ~/Templates/* -t ~/Downloads/RayRays_dotfiles/Main_PC_scripts/
+
+cd ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
+konsave --save ray --force   
+
+cd ~/Downloads/RayRays_dotfiles/
+
+git add * 
+git commit -m "script push"
+git push
