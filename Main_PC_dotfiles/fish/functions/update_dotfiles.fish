f017c54e0996fed0a18d466f91c22d6de59532a3
RayVa0 <evaone0001@gmail.com>
2025-07-01 18:52:33 +0300

    script push

---
+++ Main_PC_dotfiles/fish/functions/update_dotfiles.fish
@@ -0,0 +1,4 @@
+function update_dotfiles --wraps='sh ~/Templates/update_dotfiles.fish' --description 'alias update_dotfiles sh ~/Templates/update_dotfiles.fish'
+  sh ~/Templates/update_dotfiles.fish $argv
+        
+end
