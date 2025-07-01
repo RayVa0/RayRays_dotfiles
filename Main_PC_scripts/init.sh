2d7963ceb27147d1e423420ec9650411e9cb6a48
RayVa0 <evaone0001@gmail.com>
2025-07-02 01:00:32 +0300

    script push

--- Main_PC_scripts/init.sh
+++ Main_PC_scripts/init.sh
@@ -4,7 +4,7 @@ echo -e "\n\e[1;32mWelcome $USER\e[0m"
 
 if [ $(date +"%A") = "Saturday" ]; then 
   yay
-  sudo pacman -Scc
+  yay -Scc
 fi
 
 /usr/lib/plasma-dbus-run-session-if-needed /usr/bin/startplasma-wayland
