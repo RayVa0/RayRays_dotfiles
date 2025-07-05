f2edaf4915c332b250d46ba9cbe617021d877024
RayVa0 <evaone0001@gmail.com>
2025-07-05 18:41:22 +0300

    script push

--- Main_PC_scripts/update_dotfiles.fish
+++ Main_PC_scripts/update_dotfiles.fish
@@ -1,4 +1,4 @@
-cp -r ~/.config/kitty/ ~/.config/fastfetch/ ~/.config/fish ~/.config/hypr ~/.config/mpv -t ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
+cp -r ~/.config/Thunar/ ~/.config/kitty/ ~/.config/fastfetch/ ~/.config/fish ~/.config/hypr ~/.config/mpv -t ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
 cp ~/Templates/* -t ~/Downloads/RayRays_dotfiles/Main_PC_scripts/
 
 cd ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
