e92e3f46439fa1774d806ab9dffb4407249579cd
RayVa0 <evaone0001@gmail.com>
2025-07-05 18:34:26 +0300

    script push

---
+++ Main_PC_scripts/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <application_name>"
+    exit 1
+fi
+
+if [ "$1" = "handbrake" ]; then 
+    nohup "ghb" &
+elif [ "$1" = "plex" ]; then
+    sh ~/Downloads/run_plex.sh
+else 
+    nohup "$1" &
+fi
+
+if [ $? -gt 0 ]; then 
+    echo Opening application failed
+fi
+
+exit 0
