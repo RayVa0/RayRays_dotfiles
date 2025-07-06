56e9d13710e94216aa88567a039ed934e0159385
RayVa0 <evaone0001@gmail.com>
2025-07-06 18:31:36 +0300

    script push

--- Main_PC_dotfiles/fish/config.fish
+++ Main_PC_dotfiles/fish/config.fish
@@ -54,4 +54,27 @@ if status is-login
       sh ~/Templates/init.sh
 end 
 
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
 
