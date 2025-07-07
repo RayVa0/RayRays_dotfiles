f98ca1697f3609d88d57008ae72bb7c510e3d85e
RayVa0 <evaone0001@gmail.com>
2025-07-07 18:13:27 +0300

    scripted push

--- Main_PC_dotfiles/fish/functions/kdekill.fish
+++ Main_PC_dotfiles/fish/functions/kdekill.fish
@@ -1,4 +1,4 @@
-function kdekill --wraps='killall steam && qdbus6 org.kde.Shutdown /Shutdown logout' --wraps='qdbus6 org.kde.Shutdown /Shutdown logout' --description 'alias kdekill qdbus6 org.kde.Shutdown /Shutdown logout'
+function kdekill --wraps='qdbus6 org.kde.Shutdown /Shutdown logout' --description 'alias kdekill qdbus6 org.kde.Shutdown /Shutdown logout'
   qdbus6 org.kde.Shutdown /Shutdown logout $argv
         
 end
--- Main_PC_scripts/agpfetch.sh
+++ Main_PC_scripts/agpfetch.sh
@@ -1,3 +1,16 @@
 #!/bin/env bash
 
-sh ~/Templates/imagefetch.sh ~/Pictures/Photos/Homework/Not_Vanilla/Gender/MtF/ 2
+dir="/mnt/SATA_SSD/Photos/Homework/Not_Vanilla/Gender/MtF/"
+pic=$(find $dir -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.tiff" -o -iname "*.bmp" -o -iname "*.jpeg" -o -iname "*.webp" \) | shuf -n 1)
+
+color=8
+
+if [[ $COLORTERM =~ ^(truecolor|24bit)$ ]]; then
+	color="full"
+fi
+
+chafa -O 0 -c $color "$pic"
+echo -n "You are viewing "
+~/Documents/code/experiments/find_file "$pic"
+    
+
