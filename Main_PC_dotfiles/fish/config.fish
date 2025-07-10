28f891543c8a57296035f2bf8cf13ee0da4d7e67
RayVa0 <evaone0001@gmail.com>
2025-07-10 22:47:29 +0300

    scripted push

--- Main_PC_dotfiles/fish/config.fish
+++ Main_PC_dotfiles/fish/config.fish
@@ -8,17 +8,19 @@ function fish_greeting
     set y (random 0 1 100) 
 
     if test $y -lt 50 #50% chance for a message
-
-        set x (random choice (cat ~/.config/fish/greet_text.txt))
-
-        set z (random 0 1 100)
-        if test $z -le 90
-            set_color f5f5dc; echo -n Quote of the instance: \n\"$x\" \n
-        else 
-            echo -n Quote of the instance:\n\"$x\" \n | lolcat
-        end
+        textfetch
     else #50% it is an image
-        set z (random 0 1 100)
+        mediafetch
+    end 
+    
+end
+
+if status is-login
+      sh ~/Templates/init.sh
+end 
+
+function mediafetch
+    set z (random 0 1 100)
         if test $z -gt 75 #1% it is from the repo 
             set g (random 0 1 125000) #g for gooning
             if test $g -eq 1 #1 in a million agpfetch
@@ -44,13 +46,18 @@ function fish_greeting
             set_color 7057ff; echo --- A nice piece of art: ---
             sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/Art 3
         end
-    end 
-    
 end
 
-if status is-login
-      sh ~/Templates/init.sh
-end 
+function textfetch
+ set x (random choice (cat ~/.config/fish/greet_text.txt))
+
+    set z (random 0 1 100)
+    if test $z -le 90
+        set_color f5f5dc; echo -n Quote of the instance: \n\"$x\" \n
+    else 
+        echo -n Quote of the instance:\n\"$x\" \n | lolcat
+    end
+end
 
 function bind_bang
     switch (commandline -t)[-1]
@@ -79,4 +86,3 @@ end
 function find_edit --wraps nvim --description 'Searches for a file and opens it in nvim'
     sudo nvim $(whereis $argv | gawk '{ print $2 };' )
 end
-
