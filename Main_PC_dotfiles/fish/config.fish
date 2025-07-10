201c476f0a83521f55b9f20d71c7cbb34ecccd17
RayVa0 <evaone0001@gmail.com>
2025-07-10 23:12:53 +0300

    scripted push

--- Main_PC_dotfiles/fish/config.fish
+++ Main_PC_dotfiles/fish/config.fish
@@ -4,7 +4,6 @@ alias yay-clean 'yay --answerdiff None --answerclean All'
 
 
 function fish_greeting
-
     set y (random 0 1 100) 
 
     if test $y -lt 50 #50% chance for a message
@@ -21,8 +20,9 @@ end
 
 function mediafetch
     set z (random 0 1 100)
-        if test $z -gt 75 #1% it is from the repo 
+        if test $z -gt 66 #33% it is from the repo 
             set g (random 0 1 125000) #g for gooning
+            set a (random 1 2) #a for art 
             if test $g -eq 1 #1 in a million agpfetch
                 set_color brred; echo -n 'Warning ' 
                 command sleep 1
@@ -32,19 +32,19 @@ function mediafetch
                 command sleep 1
                 sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/Homework/Vanilla 3
                 set_color brmagenta; echo There is a one in a million chance of the shell initiating with a pornographic image such as this one!
-            else
+            else if test $a -eq 1 
                 set_color brmagenta; echo --- Todays peculiar Repo delight: --- 
-                sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/The_Repo/ 3 
+                sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/The_Repo/ 3
+            else
+                set_color 7057ff; echo --- A nice piece of art: ---
+                sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/Art 3
             end
-        else if test $z -gt 50 #50/50 from video games or media
+        else if test $z -gt 33 #33% media
             set_color 20dfdf; echo --- A nice media image: ---
             sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Screen\ Captures/Media\ Captures 1 
-        else if test $z -gt 25
+        else #33% games 
             set_color green; echo --- A nice Video Game image: ---
             sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Screen\ Captures/Game\ Captures 1
-        else
-            set_color 7057ff; echo --- A nice piece of art: ---
-            sh ~/Templates/imagefetch.sh /mnt/SATA_SSD/Photos/Art 3
         end
 end
 
