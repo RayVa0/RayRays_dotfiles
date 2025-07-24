56a02e436078d550c235f3dc16af54b054dc80f1
RayVa0 <dimitris.karagounis1@gmail.com>
2025-07-24 16:16:52 +0300

    scripted push from MacBook Pro

--- Macbook_dotfiles/fish/functions/run.fish
+++ Macbook_dotfiles/fish/functions/run.fish
@@ -1,4 +1,4 @@
-function run
-  hyprctl dispatch exec $argv
-  exit
+function run --wraps='sh ~/Templates/run.sh' --description 'alias run sh ~/Templates/run.sh'
+  sh ~/Templates/run.sh $argv
+        
 end
