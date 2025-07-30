6515ed1a7237ebb97f1db2b01bb2a01b777aaf50
RayVa0 <evaone0001@gmail.com>
2025-07-31 01:44:28 +0300

    scripted push

---
+++ Main_PC_dotfiles/fish/functions/transfetch.fish
@@ -0,0 +1,4 @@
+function transfetch --wraps='imagefetch ~/Pictures/Photos/TTTT_Repo/' --description 'alias transfetch imagefetch ~/Pictures/Photos/TTTT_Repo/'
+  imagefetch ~/Pictures/Photos/TTTT_Repo/ $argv
+        
+end
