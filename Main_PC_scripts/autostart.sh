ece7ce2d666adac22535740ac0f7f609b63b934e
RayVa0 <evaone0001@gmail.com>
2025-07-31 21:25:35 +0300

    scripted push

--- Main_PC_scripts/autostart.sh
+++ Main_PC_scripts/autostart.sh
@@ -2,10 +2,12 @@
 
 sleep 1
 
-sh ~/Templates/auto_minimize.sh viber 
-sh ~/Templates/auto_minimize.sh vesktop 
-sh ~/Templates/auto_minimize.sh waterfox 
-sh ~/Templates/auto_minimize.sh steam-native
+sh ~/Templates/auto_minimize.sh steam-native&
+sh ~/Templates/auto_minimize.sh vesktop&
+sh ~/Templates/auto_minimize.sh viber&
+sh ~/Templates/auto_minimize.sh waterfox& 
+sh ~/Templates/auto_minimize.sh spotify&
+
 
 
 
