122210ce211b500cfc71c6d40137fd3f253408fd
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-05 22:32:40 +0300

    scripted push

--- Main_PC_scripts/autostart.sh
+++ Main_PC_scripts/autostart.sh
@@ -3,11 +3,10 @@
 sleep 0.5
 
 sh ~/Templates/auto_minimize.sh steam-native&
-sh ~/Templates/auto_minimize.sh vesktop&
 sh ~/Templates/auto_minimize.sh waterfox& 
 sh ~/Templates/auto_minimize.sh spotify&
-sleep 15
-sh ~/Templates/auto_minimize.sh fluffychat&
+sleep 10
+sh ~/Templates/auto_minimize.sh ellement-desktop&
 
 
 
---
+++ Main_PC_scripts/logout.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+fish ~/Templates/update_dotfiles.fish
+killall steam
+kdotool set_desktop 1
