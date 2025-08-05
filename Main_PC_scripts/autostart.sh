9f25b8949bb0d8135c0cd57aa4bda46ee61613ca
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-05 22:34:43 +0300

    scripted push

--- Main_PC_scripts/autostart.sh
+++ Main_PC_scripts/autostart.sh
@@ -2,11 +2,10 @@
 
 sleep 0.5
 
+sh ~/Templates/auto_minimize.sh element-desktop&
 sh ~/Templates/auto_minimize.sh steam-native&
 sh ~/Templates/auto_minimize.sh waterfox& 
 sh ~/Templates/auto_minimize.sh spotify&
-sleep 10
-sh ~/Templates/auto_minimize.sh element-desktop&
 
 
 
