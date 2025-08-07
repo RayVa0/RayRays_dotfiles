13c2b3df9c7d73e04a9783d054c3ed74bdcb23ad
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-07 19:44:51 +0300

    scripted push at UNIX epoch time: 1754585091 from Ada

--- Main_PC_dotfiles/fish/functions/find_delete.fish
+++
@@ -1,4 +0,0 @@
-function find_delete --wraps='sh ~/Templates/find_del.sh -e' --description 'alias find_delete sh ~/Templates/find_del.sh -e'
-  sh ~/Templates/find_del.sh -e $argv
-        
-end
---
+++ Main_PC_dotfiles/fish/functions/yaydel.fish
@@ -0,0 +1,4 @@
+function yaydel --wraps='sh ~/Templates/find_del.sh -e' --description 'alias yaydel sh ~/Templates/find_del.sh -e'
+  sh ~/Templates/find_del.sh -e $argv
+        
+end
