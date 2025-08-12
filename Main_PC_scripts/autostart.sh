e058ed178808e3b8822b6c7ff00380bbe45eeb2d
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-12 16:05:19 +0300

    scripted push at UNIX epoch time: 1755003919 from Ada

--- Main_PC_scripts/autostart.sh
+++ Main_PC_scripts/autostart.sh
@@ -3,12 +3,12 @@
 loginctl lock-session
 
 sh ~/Templates/auto_minimize.sh steam-native&
-sh ~/Templates/auto_minimize.sh waterfox& 
 sh ~/Templates/auto_minimize.sh spotify&
 sh ~/Templates/run.sh qbittorrent&
 sh ~/Templates/auto_minimize.sh viber&
 sleep 1
 sh ~/Templates/auto_minimize.sh element-desktop&
 sh ~/Templates/auto_minimize.sh vesktop&
+sh ~/Templates/auto_minimize.sh waterfox& 
 
 exit 0
