cbb4ba377c568cbac1a58e5736d8ff7d4b5d9b14
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-14 11:58:39 +0300

    Thu Aug 14 11:58:39 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/update_dotfiles.fish
+++ Main_PC_scripts/update_dotfiles.fish
@@ -15,32 +15,12 @@ end
 set_color yellow; echo -dotfiles being copied-
 set_color normal  
 rm -rf ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/*
-cp -r ~/.config/nvim/ ~/.config/qBittorrent/ ~/.config/dolphinrc ~/.config/kitty/ ~/.config/unifetch/ ~/.config/fish ~/.config/hypr ~/.config/mpv -t ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
+cp -r ~/.config/nvim/ ~/.config/ray.knsv ~/.config/qBittorrent/ ~/.config/dolphinrc ~/.config/kitty/ ~/.config/unifetch/ ~/.config/fish ~/.config/hypr ~/.config/mpv -t ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
 
 set_color yellow; echo -scripts being copied-
 set_color normal  
 rm -rf ~/Downloads/RayRays_dotfiles/Main_PC_scripts/*
 cp ~/Templates/* -t ~/Downloads/RayRays_dotfiles/Main_PC_scripts/
-
-set_color yellow; echo -konsave-
-set_color normal  
-cd ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
-konsave --save ray --force 
-
-if test $status -ne 0
-  set_color red; echo konsave save fail 
-  exit 1
-end
-
-set_color yellow; echo -konsave export-
-set_color normal  
-cd ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/
-konsave -e ray
-
-if test $status -ne 0
-  set_color red; echo konsave export fail
-  exit 1
-end
 
 cd ~/Downloads/RayRays_dotfiles/
 
@@ -48,7 +28,6 @@ set_color yellow; echo -package list text file-
 set_color normal  
 yay -Qe > ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/explicitly_installed_packages.txt
 yay -Qe > ~/Downloads/RayRays_dotfiles/Main_PC_dotfiles/installed_packages.txt
-
 
 set date (date)
 
