4ca35e8a248c588ea367e31f450a672ec9b6c065
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-14 11:54:47 +0300

    Thu Aug 14 11:54:46 AM EEST 2025 from Ada

--- Main_PC_scripts/update_dotfiles.fish
+++ Main_PC_scripts/update_dotfiles.fish
@@ -57,7 +57,7 @@ set_color normal
 git add *
 git diff --cached --quiet 
 if test $status -ne 0
-    git commit -m "scripted push at $date from Ada"
+    git commit -m "$date from Ada"
     git push
 end
 
