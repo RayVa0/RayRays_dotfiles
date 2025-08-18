d5b202434c1e657f732bd5e52439fdb05fadddfc
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-18 13:15:14 +0300

    Mon Aug 18 01:15:14 PM EEST 2025 from Ada(script)

--- Main_PC_dotfiles/fish/functions/agpfetch.fish
+++ Main_PC_dotfiles/fish/functions/agpfetch.fish
@@ -1,4 +1,4 @@
-function agpfetch --wraps='sh ~/Templates/imagefetch_2.0.sh --no-depth --filename --conf ~/.config/imagefetch1.conf' --description 'alias agpfetch sh ~/Templates/imagefetch_2.0.sh --no-depth --filename --conf ~/.config/imagefetch1.conf'
-  sh ~/Templates/imagefetch_2.0.sh --no-depth --filename --conf ~/.config/imagefetch1.conf $argv
+function agpfetch --wraps='sh ~/Templates/imagefetch_2.0.sh --no-depth --filename --conf ~/.config/imagefetch1.conf' --wraps='sh ~/Templates/imagefetch_2.1.sh --no-depth --filename --conf ~/.config/imagefetch1.conf' --description 'alias agpfetch sh ~/Templates/imagefetch_2.1.sh --no-depth --filename --conf ~/.config/imagefetch1.conf'
+  sh ~/Templates/imagefetch_2.1.sh --no-depth --filename --conf ~/.config/imagefetch1.conf $argv
         
 end
