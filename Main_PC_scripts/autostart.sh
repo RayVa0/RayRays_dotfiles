2b71cf8c1199f4d00643cedccbff9f7318c4421e
RayVa0 <dimitris.karagounis1@gmail.com>
2025-08-19 00:40:35 +0300

    Tue Aug 19 12:40:35 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/autostart.sh
+++ Main_PC_scripts/autostart.sh
@@ -9,12 +9,10 @@ sh ~/Templates/auto_minimize.sh steam-native&
 sleep 0.1
 sh ~/Templates/run.sh qbittorrent&
 sleep 0.1
-sh ~/Templates/auto_minimize.sh element-desktop&
-sleep 0.1
-sh ~/Templates/auto_minimize.sh waterfox&
-sleep 0.1
-sh ~/Templates/auto_minimize.sh spotify&
-sleep 0.1
+sh ~/Templates/auto_minimize.sh element-desktop
+sh ~/Templates/auto_minimize.sh waterfox
+sh ~/Templates/auto_minimize.sh spotify
 
 
 exit 0
+
