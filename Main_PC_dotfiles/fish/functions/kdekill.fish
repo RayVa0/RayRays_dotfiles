8aef07a1d254280994b0f5ac53c7da11c51b1973
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-04 09:34:24 +0300

    Thu Sep  4 09:34:24 AM EEST 2025 from Ada(script)

--- Main_PC_dotfiles/fish/functions/kdekill.fish
+++ Main_PC_dotfiles/fish/functions/kdekill.fish
@@ -1,4 +1,4 @@
-function kdekill --wraps='qdbus6 org.kde.Shutdown /Shutdown logout' --wraps='loginctl terminate-user (whoami)' --description 'alias kdekill loginctl terminate-user (whoami)'
-  loginctl terminate-user (whoami) $argv
+function kdekill --wraps='loginctl terminate-user ray' --description 'alias kdekill loginctl terminate-user ray'
+  loginctl terminate-user ray $argv
         
 end
