d9a63ca4a16f71b20f16e2ea37df36dfecfe12b1
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-09 09:38:36 +0300

    Tue Sep  9 09:38:36 AM EEST 2025 from Ada(script)

---
+++ Main_PC_dotfiles/fish/functions/repofetch.fish
@@ -0,0 +1,4 @@
+function repofetch --wraps='c && imagefetch.sh ~/Pictures/Photos/The_Repo/' --description 'alias repofetch c && imagefetch.sh ~/Pictures/Photos/The_Repo/'
+  c && imagefetch.sh ~/Pictures/Photos/The_Repo/ $argv
+        
+end
