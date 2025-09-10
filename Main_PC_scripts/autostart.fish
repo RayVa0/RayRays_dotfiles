a89ad3e8a8ff53729543b73e21e2db404cd91e68
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-11 00:19:41 +0300

    Thu Sep 11 12:19:41 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/autostart.fish
+++ Main_PC_scripts/autostart.fish
@@ -5,7 +5,7 @@
 loginctl lock-session
 kdotool set_desktop 1
 
-plasma-apply-wallpaperimage (find /home/ray/Pictures/Wallpapers/ | shuf -n 1)
+plasma-apply-wallpaperimage (plocate /home/ray/Pictures/Wallpapers/ | grep -iE '(jpeg|jpg|png)' | shuf -n 1)
 sh ~/Templates/auto_minimize.sh steam-native&
 sh ~/Templates/run.sh qbittorrent&
 sh ~/Templates/auto_minimize.sh element-desktop&
