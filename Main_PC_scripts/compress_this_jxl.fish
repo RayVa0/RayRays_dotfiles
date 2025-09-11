6564e8e44530ec1502fa46a207d169e525cede57
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-11 11:41:57 +0300

    Thu Sep 11 11:41:57 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/compress_this_jxl.fish
+++ Main_PC_scripts/compress_this_jxl.fish
@@ -1,16 +1,22 @@
 #!/usr/bin/env fish
 mkdir output
-set size (find ./ -type f -size +1M | grep -iE "(jpg|jpeg|png|webp|jxl|avif)" | wc -l)
+set size (find ./ -type f -size +1024k | grep -iE "(jpg|jpeg|png|webp)" | wc -l)
 set count 1
 
-for i in (find ./ -type f -size +1M | grep -iE "(jpg|jpeg|png|webp|jxl|avif)")
+for i in (find ./ -type f -size +1024k | grep -iE "(jpg|jpeg|png|webp)")
   set name (echo $i | sed 's/\.[^.]*$//' | cut -c 3-)
   cjxl --quiet -d 1 -e 8 --lossless_jpeg=0 --faster_decoding=4 $i output/$name.jxl
+  if test $status -ne 0
+    set_color red; echo an encode failed copying file as was
+    cp -v $i output/
+    set_color normal
+  end
+
   echo Compressing -- $count/$size
   set count (math "$count + 1")
 end
 
-for i in (find ./ -type f -size -1M | grep -iE "(jpg|jpeg|png|webp|jxl|avif)")
+for i in (find ./ -type f -size -1024k | grep -iE "(jpg|jpeg|png|webp|jxl|avif)")
   cp -v $i output/
 end
 
