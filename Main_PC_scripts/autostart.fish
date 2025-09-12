3989a52859af9cb60f7d877d7e28953815ba9b5c
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-12 10:05:35 +0300

    Fri Sep 12 10:05:35 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/autostart.fish
+++ Main_PC_scripts/autostart.fish
@@ -5,6 +5,8 @@
 loginctl lock-session
 kdotool set_desktop 1
 
+net #check for network
+
 plasma-apply-wallpaperimage (plocate /home/ray/Pictures/Wallpapers/ | grep -iE '(jpeg|jpg|png)' | shuf -n 1)
 sh ~/Templates/auto_minimize.sh steam-native&
 sh ~/Templates/run.sh qbittorrent&
@@ -12,7 +14,7 @@ sh ~/Templates/auto_minimize.sh element-desktop&
 sh ~/Templates/auto_minimize.sh waterfox&
 sh ~/Templates/auto_minimize.sh spotify&
 auto_minimize.sh viber&
-auto_minimize vesktop&
+auto_minimize.sh vesktop&
 
 pinging && update_dotfiles
 
