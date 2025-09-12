2c8c901b5bdb64610d6601cc46aae05666a2cf78
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-12 23:43:49 +0300

    Fri Sep 12 11:43:49 PM EEST 2025 from Ada(script)

--- Main_PC_scripts/compress_this_jxl.fish
+++ Main_PC_scripts/compress_this_jxl.fish
@@ -5,7 +5,7 @@ set count 1
 
 for i in (find ./ -type f -size +1024k | grep -iE "(jpg|jpeg|png|webp)")
   set name (echo $i | sed 's/\.[^.]*$//' | cut -c 3-)
-  cjxl --quiet -d 1 -e 7 --lossless_jpeg=0 --faster_decoding=4 $i output/$name.jxl
+  cjxl --quiet -d 1 -e 10 --lossless_jpeg=0 --faster_decoding=4 $i output/$name.jxl
   if test $status -ne 0
     set_color red; echo an encode failed copying file as was
     cp -v $i output/
@@ -16,7 +16,7 @@ for i in (find ./ -type f -size +1024k | grep -iE "(jpg|jpeg|png|webp)")
   set count (math "$count + 1")
 end
 
-for i in (find ./ -type f -size -1024k | grep -iE "(jpg|jpeg|png|webp|jxl|avif)")
-  cp $i output/
+for i in (find ./ -maxdepth 1 -type f -size -1024k | grep -iE "(jpg|jpeg|png|webp|jxl|avif)")
+  cp -v $i output/
 end
 
