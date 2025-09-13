d8f86ad7ee47ee3d43ba806f30d761a48e5e3c0f
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-13 10:31:45 +0300

    Sat Sep 13 10:31:45 AM EEST 2025 from Ada(script)

--- Main_PC_scripts/compress_this_jxl.fish
+++ Main_PC_scripts/compress_this_jxl.fish
@@ -5,6 +5,9 @@ set count 1
 
 for i in (find ./ -type f -size +1024k | grep -iE "(jpg|jpeg|png|webp)")
   set name (echo $i | sed 's/\.[^.]*$//' | cut -c 3-)
+  set name_alt (echo $i | sed 's/\.[^.]*$//')
+  mkdir -p output/(dirname $name_alt.jxl)
+  touch $name.jxl
   cjxl --quiet -d 1 -e 10 --lossless_jpeg=0 --faster_decoding=4 $i output/$name.jxl
   if test $status -ne 0
     set_color red; echo an encode failed copying file as was
