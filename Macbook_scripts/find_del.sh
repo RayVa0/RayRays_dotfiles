9b32f89c44c19f664dfa23de315a46fdc37a7e7d
RayVa0 <dimitris.karagounis1@gmail.com>
2025-09-30 22:48:02 +0300

    scripted push at UNIX epoch time 1759261682 from MacBook Pro

--- Macbook_scripts/find_del.sh
+++ Macbook_scripts/find_del.sh
@@ -5,21 +5,45 @@ user_satisfied=0
 line_number=1
 explicit="-e"
 
+if [ $# -eq 2 ] && [ "$1" = "$explicit" ]; then
+  echo -n
+elif [ $# -ge 2 ]; then 
+  echo Please pass only one package for deletion.
+  exit 1
+elif [ $# -lt 2 ] && [ "$1" = "$explicit" ]; then 
+  echo Please pass a package for deletion
+  exit 1
+fi
 
-if [ "$1" = "$explicit" ]; then  
+if [ $# -lt 1 ]; then
+  echo No arguments passed
+  exit 1
+fi
+
+if [ "$1" = "$explicit" ]; then 
+  echo ---Explicit Mode---
   initial_check=$(yay -Q | grep $2 | wc -l)
   if [ $initial_check -eq 0 ]; then 
     echo No matches found 
     exit 1
   fi
 
-  echo matching packages
+  echo Matching packages:
+  echo 
   yay -Q | grep $2 | awk '{printf "%d: %s\n", NR, $0}'
-  echo -n Which one do you want gone?
+  echo
+  echo -n 'Which one do you want gone(q to exit)?'
   read -p "" selected
+
+  if [ "$selected" = "q" ]; then 
+    echo exiting...
+    exit 0
+  fi
+
   temp=$(yay -Q | grep $2 | awk "NR==$selected")
   to_delete=$(echo $temp | cut -f1 -d" ")
 else
+  echo '---Simple Mode(deprecated)---'
   initial_check=$(yay -Q | grep $1 | wc -l)
   if [ $initial_check -eq 0 ]; then 
     echo No matches found 
@@ -41,6 +65,11 @@ else
     esac
 
   line_number=$((line_number+1))
+
+  if [ $line_number -gt $initial_check ]; then 
+    echo No more packages, exiting.
+    exit 0
+  fi
     
   done 
 fi
