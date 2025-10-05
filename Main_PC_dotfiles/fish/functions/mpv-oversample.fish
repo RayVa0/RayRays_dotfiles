a4b17202b8ea82b2dfedd75f933bc16742e24a42
RayVa0 <dimitris.karagounis1@gmail.com>
2025-10-05 11:39:18 +0300

    Sun Oct  5 11:39:18 AM EEST 2025 from Ada(script)

---
+++ Main_PC_dotfiles/fish/functions/mpv-oversample.fish
@@ -0,0 +1,3 @@
+function mpv-oversample --wraps='mpv --scale=oversample' --description 'alias mpv-oversample mpv --scale=oversample'
+    mpv --scale=oversample $argv
+end
--- Main_PC_scripts/autostart.fish
+++ Main_PC_scripts/autostart.fish
@@ -3,7 +3,7 @@
 
 casual_display.fish
 hdr-off
-crt-off
+CRT-off
 loginctl lock-session&
 kdotool set_desktop 1
 #plasma-apply-wallpaperimage (plocate /home/ray/Pictures/Wallpapers/ | grep -iE '(jpeg|jpg|png)' | shuf -n 1)
