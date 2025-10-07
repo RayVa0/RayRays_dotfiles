aeb7d024542fb1d4714cc3afe048ca37ed5717cb
RayVa0 <dimitris.karagounis1@gmail.com>
2025-10-07 22:24:45 +0300

    Tue Oct  7 10:24:45 PM EEST 2025 from Ada(script)

---
+++ Main_PC_dotfiles/fish/functions/lcd.fish
@@ -0,0 +1,13 @@
+#longman cd function
+
+function lcd --description 'Changes Directory & Shows the files inside of it'
+    if test -f $argv
+        set argv ~
+    end
+    if [ (count (ls $argv)) -lt "20" ]
+        ls $argv
+    else
+        set_color brmagenta; echo "File Count: "(count (ls $argv))
+    end
+    builtin cd $argv
+end
